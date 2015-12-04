1d5da8d2d3bf15ec967c1eceaddfad5578a52eb8
Philip Choi <grjkz0@gmail.com>
2015-12-03 22:24:32 -0500

    added awesome prev/next nagivation in show_episode.ejs | now working on admin edit pages

--- routes/admin.js
+++ routes/admin.js
@@ -2,15 +2,49 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db/sql.js');
 
+
+///////////
+// ADMIN //
+///////////
+
+
+// show all admin controls
+router.get('/', function(req,res,next) {
+	res.render('admin/index');
+});
+
+
+// add new content
 router.get('/content/new', function(req,res,next) {
 
 	db.query("SELECT * FROM series;", function(err, results, fields) {
-		res.render('add_content', {
+		res.render('admin/add_content', {
 			page: 'add_content',
 			allSeries: results
 		});
 	});
 
+});
+
+
+// show all series (links ahead to edit respective episodes)
+router.get('/series', function(req,res,next) {
+	db.query("SELECT * FROM series;", function(err,results,fields) {
+		res.render('admin/show_series', {
+			page: 'show_series',
+			series: results});
+	});
+});
+
+
+// edit episodes of a series
+router.get('/series/:series_id', function(req,res,next) {
+	db.query("SELECT * FROM episodes as e WHERE e.series_id = ?", req.params.series_id, function(err, results, fields) {
+		res.render('/admin/edit_episodes', {
+			page: 'show_episodes',
+			episodes: results
+		});
+	});
 });
 
 module.exports = router;
--- routes/series.js
+++ routes/series.js
@@ -8,6 +8,7 @@ var db = require('../db/sql.js');
 ////////////
 
 
+// show all series
 router.get('/', function(req,res,next) {
 	// return all series with number of episodes, even if zero episodes
 	db.query("SELECT s.*, count(e.episode_id) as episodes FROM series as s LEFT JOIN episodes as e ON s.series_id = e.series_id GROUP BY s.series_id ORDER BY name;", function(err, results, fields) {
@@ -19,6 +20,7 @@ router.get('/', function(req,res,next) {
 });
 
 
+// show all episodes in a series
 router.get('/:series_id', function(req,res,next) {
 	db.query("SELECT * FROM series as s, episodes as e WHERE s.series_id = e.series_id AND s.series_id = ? ORDER BY e.ep_index;", req.params.series_id, function(err, results, fields) {
 		// get all episodes of specific series
@@ -30,11 +32,47 @@ router.get('/:series_id', function(req,res,next) {
 });
 
 
-router.get('/:series_id/episode/:ep_num', function(req,res,next) {
-	db.query("SELECT * FROM series as s, episodes as e WHERE s.series_id = e.series_id AND s.series_id = ? AND e.ep_num = ? LIMIT 1;", [req.params.series_id, req.params.ep_num], function(err, result, fields) {
-		res.render('show_episode', {
-			page: 'show_episode',
-			episode: result[0]
+// show single episode
+router.get('/:series_id/episode/:ep_id', function(req,res,next) {
+	db.query("SELECT * FROM series as s, episodes as e WHERE s.series_id = e.series_id AND s.series_id = ? AND e.episode_id = ? LIMIT 1;", [req.params.series_id, req.params.ep_id], function(err, episode, fields) {
+		// console.log(episode[0].ep_index)
+
+		var epIndex = episode[0].ep_index;
+		// get previous and next episodes
+		db.query("Select series_id,ep_index,ep_num,title FROM episodes "+
+			"WHERE ("+
+				"ep_index = IFNULL((SELECT min(ep_index) FROM episodes WHERE ep_index > ?),0) "+
+				"OR  ep_index = IFNULL((SELECT max(ep_index) from episodes WHERE ep_index < ?),0)"+
+			") "+
+			"AND (series_id = ?);", [epIndex, epIndex, req.params.series_id], function(err2,prevNextEps,fields2) {
+			// console.log(prevNextEps)
+			
+			var prevIndex = "null";
+			var nextIndex = "null";
+
+			// check to make sure other episodes exist
+			if (prevNextEps.length === 2) {
+				// first returned index (prevNextEps[0]) is always the lower number
+				prevIndex = prevNextEps[0];
+				// second returned index (prevNextEps[1]) is always the higher number
+				nextIndex = prevNextEps[1];
+			} else if (prevNextEps.length === 1) {
+				// check if result is lower or higher than searched ep_index
+				if (prevNextEps[0].ep_index < epIndex) { // lower
+					prevIndex = prevNextEps[0];
+				} else { // higher
+					nextIndex = prevNextEps[0];
+				}
+			}
+
+			// console.log(prevIndex, nextIndex)
+			res.render('show_episode', {
+				page: 'show_episode',
+				episode: episode[0],
+				prevEpIndex: prevIndex,
+				nextEpIndex: nextIndex
+			});
+			
 		});
 	});
 });
