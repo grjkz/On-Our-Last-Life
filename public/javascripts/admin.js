8cbbcea39c48d9aa75ffd159cc6ec3395813e5ca
Philip Choi <grjkz0@gmail.com>
2015-12-07 03:55:14 -0500

    deleted .focusout functions since they dont work

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,33 +1,6 @@
 console.log('admin.js linked');
 $(function() {
 
-	/**
-	 
-	 * Makes mandatory empty fields red on focusout
-	 
-	 */
-
-	$('.mandatory-field').focusout(function() {
-		if ( !$(this).val().trim() ) {
-			$(this).addClass('error');
-		}
-		else {
-			$(this).removeClass('error');
-		}
-	});
-
-
-	/**
-	 
-	 * Checks new series name value for duplicate
-	 
-	 */
-	
-	 $('.db-unique').focusout(function() {
-	 	if (checkDuplicateSeries( this.value.trim() )) {
-	 		$(this).addClass('error');
-	 	}
-	 });
 
 
 });
@@ -47,20 +20,4 @@ function globalMessage(message) {
 												delay: 3000,
 												duration: 500
 											});
-}
-
-
-/**
- * Check for duplicate Series Name in #ep-series > option tags
- * @param  {string} name Series Name to check against
- * @return {bool}        true if duplicate exists
- */
-function checkDuplicateSeries(name) {
-	var duped = false;
-
-	$('#ep-series > option').each(function(index, item) {
-		if (item.text === name) { dupped = true; }
-	});
-
-	return duped;
 }
