e28b5dac8ca32b43c14412a54fac18ecf3bc0610
Philip Choi <grjkz0@gmail.com>
2015-12-14 17:39:28 -0500

    admin authentication now redirects correctly | added "No episodes..." message to admin/show_episodes

--- routes/admin.js
+++ routes/admin.js
@@ -25,63 +25,63 @@ router.get('/', checkAuth, function(req,res,next) {
 // });
 
 
-// add new content
-router.get('/content/new', function(req,res,next) {
+// // add new content
+// router.get('/content/new', function(req,res,next) {
 
-	db.query("SELECT * FROM series;", function(err, results, fields) {
-		res.render('admin/add_content', {
-			page: 'add_content',
-			allSeries: results
-		});
-	});
+// 	db.query("SELECT * FROM series;", function(err, results, fields) {
+// 		res.render('admin/add_content', {
+// 			page: 'add_content',
+// 			allSeries: results
+// 		});
+// 	});
 
-});
-
-
-// show all series (links ahead to edit respective episodes)
-router.get('/series', function(req,res,next) {
-	db.query("SELECT * FROM series;", function(err,results,fields) {
-		res.render('admin/show_series', {
-			page: 'show_series',
-			series: results});
-	});
-});
+// });
 
 
-// edit episodes of a series
-router.get('/series/:series_id', function(req,res,next) {
-	db.query("SELECT * FROM episodes as e WHERE e.series_id = ?", req.params.series_id, function(err, results, fields) {
-		res.render('/admin/edit_episodes', {
-			page: 'show_episodes',
-			episodes: results
-		});
-	});
-});
+// // show all series (links ahead to edit respective episodes)
+// router.get('/series', function(req,res,next) {
+// 	db.query("SELECT * FROM series;", function(err,results,fields) {
+// 		res.render('admin/show_series', {
+// 			page: 'show_series',
+// 			series: results});
+// 	});
+// });
 
 
-// login
+// // edit episodes of a series
+// router.get('/series/:series_id', function(req,res,next) {
+// 	db.query("SELECT * FROM episodes as e WHERE e.series_id = ?", req.params.series_id, function(err, results, fields) {
+// 		res.render('/admin/edit_episodes', {
+// 			page: 'show_episodes',
+// 			episodes: results
+// 		});
+// 	});
+// });
+
+
+// admin page login
 router.get('/login', function(req,res,next) {
 	res.render('admin/admin_login', {loginMessage: req.flash('loginMessage')});
 });
 
-// admin login
 router.post('/login',
-	passport.authenticate('local-admin-login',
+	passport.authenticate(
+		'local-admin-login',
 		{
 			successRedirect: '/admin',
-			failureRedirect: '/admin/login',
-			falureFlash: false
-		}
-		), function(req,res,next) {
-	console.log(req.body)
-	res.json({body: req.body})
-});
+			failureRedirect: '/admin/login'
+		})
+);
 
 
 function checkAuth(req, res, next) {
-  if (!req.session.user_id) {
+	// check if session is stored
+  if (!req.session.passport || !req.session.passport.user || !req.user.admin) {
     res.redirect('/');
   } else {
+  	// force forget cache history so logging out and hitting 'back' wont be able to rerender the page
+  	res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+
     next();
   }
 }
