1704e2a437c7acf525583e6fea985ae2061159ac
Philip Choi <gorejakz@gmail.com>
2016-02-15 21:45:26 -0500

    cleaned up lines

--- config/passport.js
+++ config/passport.js
@@ -19,9 +19,9 @@ module.exports = function(passport) {
   passport.use('local-signup', new LocalStrategy({
   	passReqToCallback: true
   },
-  function(req, username, password, done) {
+	  function(req, username, password, done) {
 
-  }
+	  }
   ));
 
 
@@ -29,34 +29,34 @@ module.exports = function(passport) {
   passport.use('local-login', new LocalStrategy({
   	passReqToCallback: true
   },
-  function(req, username, password, done) {
-  	db.query("SELECT * FROM users WHERE user_id = ? LIMIT 1;", username, function(err, results, fields) {
-  		if (err) { return done(err); }
-  		if (!results.length || results[0].password !== password) {
-  			return done(null, false, req.flash('loginMessage', 'Username or Password is wrong'));
-  		}
-  		return done(null, rows[0]);
-  	});
-  }
-  ));
+	  function(req, username, password, done) {
+	  	db.query("SELECT * FROM users WHERE user_id = ? LIMIT 1;", username, function(err, results, fields) {
+	  		if (err) { return done(err); }
+	  		if (!results.length || results[0].password !== password) {
+	  			return done(null, false, req.flash('loginMessage', 'Username or Password is wrong'));
+	  		}
+	  		return done(null, rows[0]);
+	  	});
+	  }
+	  ));
 
 
-  // admin login
-  passport.use('local-admin-login', new LocalStrategy({
-  	passReqToCallback: true
-  },
-  function(req, username, password, done) {
-  	db.query("SELECT * FROM users WHERE admin >= 2 AND username = ? LIMIT 1;", username, function(err, result, fields) {
-  		// console.log('****************************')
-  		// console.log(result)
-  		if (err) { return done(err); }
-  		if (!result.length || result[0].password !== password) {
-  			console.log('no match')
-  			return done(null, false, req.flash('loginMessage', 'Username or Password is wrong'));
-  		}
-  		return done(null, result[0]);
-  	});
-  }
+	  // admin login
+	  passport.use('local-admin-login', new LocalStrategy({
+	  	passReqToCallback: true
+	  },
+	  function(req, username, password, done) {
+	  	db.query("SELECT * FROM users WHERE admin >= 2 AND username = ? LIMIT 1;", username, function(err, result, fields) {
+	  		// console.log('****************************')
+	  		// console.log(result)
+	  		if (err) { return done(err); }
+	  		if (!result.length || result[0].password !== password) {
+	  			console.log('no match')
+	  			return done(null, false, req.flash('loginMessage', 'Username or Password is wrong'));
+	  		}
+	  		return done(null, result[0]);
+	  	});
+	  }
   ));
 
 
